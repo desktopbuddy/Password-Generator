01ba89a586fcc54de9ed1834c9a75104df9f9c8b
desktopbuddy <hanchondo21@gmail.com>
2023-07-02 01:01:17 -0700

    added length of password prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  let passwordLength = prompt("What would you like the length of your password to be?");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be 8 to 128 characters long.")
+  } else {
+    // Name of lowercase variable
+  }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
